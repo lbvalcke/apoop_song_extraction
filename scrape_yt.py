86943550adae18550943d18157613263c14aed4a
lbvalcke <lbvalcke@gmail.com>
2020-11-28 22:41:47 -0800

    Added to scraping file

--- scrape_yt.py
+++ scrape_yt.py
@@ -1,6 +1,6 @@
 from urllib.request import Request, urlopen
 import requests
-from bs4 import BeautifulSoup as soup
+from bs4 import BeautifulSoup
 import sys
 import json
 from datetime import datetime
@@ -9,9 +9,24 @@ apoop = "https://www.youtube.com/c/milosh9k/videos"
 
 r = requests.get(apoop)
 page = r.text
-soup = soup(page,'html.parser')
-containers = soup.find_all('a',{'class':'yt-simple-endpoint inline-block style-scope ytd-thumbnail'})
+parsed_page = BeautifulSoup(page,'lxml')
+containers = parsed_page.find_all('script')
 
-for container in containers:
-    print(container)
+content = containers[28].get_text()
+stripped_content = content[20:len(content)-1]
+json_content = json.loads(stripped_content)
+
+vid_list = []
+
+for key in json_content['contents']['twoColumnBrowseResultsRenderer']['tabs']:    
+    vid_list.append(key)
+
+for vid in vid_list:
+    for key in vid:
+        print(key)
+
+
+for item in vid_list[1]['tabRenderer']['content']['sectionListRenderer']['contents'][0]['itemSectionRenderer']['contents'][0]['gridRenderer']['items']: 
+    url = item['gridVideoRenderer']['navigationEndpoint']['commandMetadata']['webCommandMetadata']['url']
+    print(url)
 
